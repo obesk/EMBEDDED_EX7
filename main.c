1bf58c3a8329d59e440147185fc1c9cf20a1a088
Roberto Bertelli <robbybertelli@gmail.com>
2025-05-28 17:05:37 +0200

    Completed first exercise
    Signed-off-by: Roberto Bertelli <robbybertelli@gmail.com>

--- main.c
+++ main.c
@@ -1,17 +1,52 @@
 #include "pwm.h"
+#include "timer.h"
 #include "xc.h"
 
-int main(void) {
+void button_init(void) {
+	RPINR0bits.INT1R = 0x58; // remapping the interrupt 1 to the T2 button pin
+	IFS1bits.INT1IF = 0;	 // resetting flag of interrupt 1
+	IEC1bits.INT1IE = 1;	 // enabling interrupt 1
+}
 
-	ANSELA = ANSELB = ANSELC = ANSELD = ANSELE = ANSELG = 0;
+void timers_init() {
+	IEC0bits.T2IE = 1; // enabling the timer 3interrupt
+
+	IFS0bits.T2IF = 0;
+}
+
+int move_forward = 0;
+
+int main(void) {
+	INTCON2bits.GIE = 1;
+
+	TRISA = TRISG = ANSELA = ANSELB = ANSELC = ANSELD = ANSELE = ANSELG =
+		0x0000;
+
 	pwm_init();
+	button_init();
+	timers_init();
 
 	move(FORWARD);
-
-	pwm_start();
 
 	while (1)
 		;
 
 	return 0;
+}
+
+void __attribute__((__interrupt__, __auto_psv__)) _INT1Interrupt(void) {
+	IFS1bits.INT1IF = 0;
+	tmr_setup_period(TIMER2, 10);
+}
+
+void __attribute__((__interrupt__, no_auto_psv)) _T2Interrupt(void) {
+	IFS0bits.T2IF = 0;
+	T2CONbits.TON = 0;
+
+	if (move_forward) {
+		pwm_start();
+	} else {
+		pwm_stop();
+	}
+	move_forward = !move_forward;
 }
